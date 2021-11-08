b703d67cf00063a9c687ff40b57e94d18c5074d0
izacpeterson <izacpeterson@gmail.com>
2021-11-08 15:08:14 -0700

    disp questions to dom

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const URL = "https://opentdb.com";
 
+//Function for fetching data
 async function fetchData(subURL, callback) {
   let rawData = await fetch(`${URL}/${subURL}`);
   let jsonData = await rawData.json();
   callback(jsonData);
 }
 
-function dispCat(jsonData) {
-  console.log(jsonData);
-  let catOpt = document.getElementById("selCat");
-  jsonData.trivia_categories.forEach((element) => {
-    catOpt.innerHTML += `<option value='${element.id}'>${element.name}</option>`;
-  });
-}
-
-fetchData("api_category.php", dispCat);
+//Fetch categories
+fetchData("api_category.php", (jsonData) => {
+  {
+    let catOpt = document.getElementById("selCat");
+    jsonData.trivia_categories.forEach((element) => {
+      catOpt.innerHTML += `<option value='${element.id}'>${element.name}</option>`;
+    });
+  }
+});
 
 export { fetchData, URL };
--- game/game.js
+++ game/game.js
@@ -1,17 +1,20 @@
 import { fetchData, URL } from "../app.js";
 
+//get trivia params from URL
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-
-console.log(params);
-
 const diff = params.diff;
 const type = params.type;
 const cat = params.cat;
 
+//fetch questions from API
+fetchData(`api.php?amount=15&difficulty=${diff}&type=${type}&category=${cat}`, getQuestions);
+
+//process returned questions
 function getQuestions(jsonData) {
   console.log(jsonData);
+  let questionList = jsonData.results;
+  questionList.forEach((el, index) => {
+    document.querySelector("#qList").innerHTML += `<h3>${index}:${el.question}</h3>`;
+  });
 }
-
-// console.log(URL + `/api.php?amount=10&type=${type}&category=${cat}`);
-fetchData(`api.php?amount=15&difficulty=${diff}&type=${type}&category=${cat}`, getQuestions);
